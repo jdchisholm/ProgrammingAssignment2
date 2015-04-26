d57fbfd672c2754dc5d66ca8fd1c969d7ddae842
jdchisholm <Gotham@192.168.2.25>
2015-04-25 23:42:56 -0300

    Assignment 2
    Comments & code

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The makeCacheMatrix takes in a matrix object but also consists of a number of other functions that can be used to set and get an entered matrix or inverse matrix. None of the functions calculates the inverse, an entered inverse matrix is just held in the "cache"
+## set function - sets the matrix, auto set when a matrix is passed into the function, but x$set can be used to set new matrix any time.
+## get function - calling this function (x$get()) will return the matrix currently set in the x$set function
+## setinv - function allows user to set/enter an inverse matrix to cache
+## getinv - calling this function will return the inverse matrix entered in x$setinv
 
-## Write a short comment describing this function
+## Function takes in a matrix, or allows users to set a new matrix at anytime. The function however does not calculate the inverse of the matrix, it just holds an inverse matrix that the user enters via x$setinv. If no value is entered into x$setinv, then the inverse variable is left as NULL
 
 makeCacheMatrix <- function(x = matrix()) {
+      m <- NULL
+      set <- function(y) {
+            x <<- y
+            m <<- NULL
+      }
+      get <- function() x
+      setinv <- function(solve) m <<- solve
+      getinv <- function() m
+      list(set = set, get = get,
+           setinv = setinv,
+           getinv = getinv)
 }
 
 
+## This function calls the getinv function from makeCacheMatrix and checks the value. If x$getinv() is not NULL (i.e., an inverse matrix was entered in makeCacheMatrix), then the function returns the cached inverse matrix. If no matrix was entered into x$setinv (i.e., x$getinv returns NULL), then the function determines and returns the inverse of whatever matrix is returned by x$get() (i.e., the matrix entered in the makeCacheMatrix function or set using x$set)   
 
 cacheSolve <- function(x, ...) {
+      m <- x$getinv()
+      if(!is.null(m)) {
+            message("getting cached data")
+            return(m)
+      }
+      data <- x$get()
+      m <- solve(data, ...)
+      x$setinv(m)
+      m
 }
